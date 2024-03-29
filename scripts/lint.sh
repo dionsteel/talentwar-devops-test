e7cabaa5c0837d8f3a29e2eec0843369e78ba4a3
Dion Steel <dions@frugl.com.au>
2024-03-29 16:59:22 +1000

    split lint call to script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 runswift=$(swift package plugin swiftlint)
 
-if [$runswift = "0"]; then
+if [ $runswift == "0" ]; then
   docker run -it -v `pwd`:`pwd` -w `pwd` ghcr.io/realm/swiftlint:latest
 fi
 
