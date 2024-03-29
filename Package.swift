20911b4b7660840d4a498965efbc3c6dc449af3e
Dion Steel <dions@frugl.com.au>
2024-03-29 16:21:32 +1000

    try to save

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DevOpsTest",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DevOpsTest",
+            targets: ["DevOpsTest"]            
+            )
+    ],
+    dependencies: [
+      .package(url: "https://github.com/realm/SwiftLint.git", from: "0.50.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DevOpsTest",
+            path: "DevOpsTest",
+            plugins: [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
+        ),
+        .testTarget(
+            name: "DevOpsTestTests", 
+            dependencies: ["DevOpsTest"],
+            path: "DevOpsTestTests"
+          ),
+    ]
+)
