2625ed5504126ebbdcef597607503688c628ff12
Dion Steel <dions@frugl.com.au>
2024-03-30 11:31:07 +1000

    save

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,14 +3,14 @@ echo "Linting source code..."
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   echo "Running swiftlint for linux with docker:"
   docker pull ghcr.io/realm/swiftlint:latest
-  docker run -t -v `pwd`:`/workdir` -w `/workdir` ghcr.io/realm/swiftlint:latest
+  docker run -it -v `pwd`:`/workdir` -w `/workdir` ghcr.io/realm/swiftlint:latest
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Running swiftlint for MacOS:"
   swift run swiftlint
 elif [[ "$OSTYPE" == "msys"* ]]; then
   set +x
   echo "Swiftlint doesn't work on windows, will attempt to docker it"
-  docker run -t -v `pwd`:`/workdir` -w `/workdir` ghcr.io/realm/swiftlint:latest
+  docker run -it -v `pwd`:`/workdir` -w `/workdir` ghcr.io/realm/swiftlint:latest
 fi
 if [ $? -eq 0 ]; then 
   echo "Linting failed."
